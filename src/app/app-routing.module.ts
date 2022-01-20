530e641ddffaa863c91690ee7e03f4a2c449a4e1
Porkoláb Ádám <adam.porkolab@gmail.com>
2022-01-20 16:18:11 +0100

    One fixing to app-routing.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventsListComponent } from './page/events-list/events-list.component';
 
 const routes: Routes = [
   {
